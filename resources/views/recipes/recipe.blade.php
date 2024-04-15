f11e915f2ad7f34f055cec9d77b91f0dc353fc8a
Bram van Bunnik <bramvanbunnik@gmail.com>
2024-04-15 20:43:59 +0100

    added ld+json

--- resources/views/recipes/recipe.blade.php
+++ resources/views/recipes/recipe.blade.php
@@ -5,75 +5,25 @@
     {
       "@context": "https://schema.org/",
       "@type": "Recipe",
-      "name": @json($recipe->title),
+      "name": {{$recipe->title}},
       "image": [
-        @json($recipe->photo)
+        http:\\recipes.home\{{$recipe->photo}}
       ],
-      "description": @json($recipe->description),
-      "recipeCuisine": @json($recipe->cuisine->name),
-      "prepTime": "PT1M",
-      "cookTime": "PT@json($recipe->cooking_time)M",
-      "totalTime": "PT3M",
-      "keywords": "non-alcoholic",
-      "recipeYield": "4 servings",
-      "recipeCategory": "Drink",
-      "nutrition": {
-        "@type": "NutritionInformation",
-        "calories": "120 calories"
-      },
-      "aggregateRating": {
-        "@type": "AggregateRating",
-        "ratingValue": "5",
-        "ratingCount": "18"
-      },
+      "description": {{$recipe->description}},
+      "recipeCuisine": {{($recipe->cuisine->name)}},
+      "cookTime": "PT{{$recipe->cooking_time}}M",
+      "totalTime": "PT{{$recipe->cooking_time}}M",
+      "recipeYield": "{{$recipe->servings}} servings",
+      "recipeCategory": "{{$recipe->course->name}}",
       "recipeIngredient": [
-        "400ml of pineapple juice",
-        "100ml cream of coconut",
-        "ice"
+        @foreach($recipe->ingredients as $ingredient)
+            {!! $ingredient->pivot->quantity !!} {!! $ingredient->units_title !!} {!! $ingredient->name !!}
+        @endforeach
       ],
       "recipeInstructions": [
-        {
-          "@type": "HowToStep",
-          "name": "Blend",
-          "text": "Blend 400ml of pineapple juice and 100ml cream of coconut until smooth.",
-          "url": "https://example.com/non-alcoholic-pina-colada#step1",
-          "image": "https://example.com/photos/non-alcoholic-pina-colada/step1.jpg"
-        },
-        {
-          "@type": "HowToStep",
-          "name": "Fill",
-          "text": "Fill a glass with ice.",
-          "url": "https://example.com/non-alcoholic-pina-colada#step2",
-          "image": "https://example.com/photos/non-alcoholic-pina-colada/step2.jpg"
-        },
-        {
-          "@type": "HowToStep",
-          "name": "Pour",
-          "text": "Pour the pineapple juice and coconut mixture over ice.",
-          "url": "https://example.com/non-alcoholic-pina-colada#step3",
-          "image": "https://example.com/photos/non-alcoholic-pina-colada/step3.jpg"
-        }
+        {{$recipe->preparation}}
       ],
-      "video": {
-        "@type": "VideoObject",
-        "name": "How to Make a Non-Alcoholic Piña Colada",
-        "description": "This is how you make a non-alcoholic piña colada.",
-        "thumbnailUrl": [
-          "https://example.com/photos/1x1/photo.jpg",
-          "https://example.com/photos/4x3/photo.jpg",
-          "https://example.com/photos/16x9/photo.jpg"
-         ],
-        "contentUrl": "https://www.example.com/video123.mp4",
-        "embedUrl": "https://www.example.com/videoplayer?video=123",
-        "uploadDate": "2018-02-05T08:00:00+08:00",
-        "duration": "PT1M33S",
-        "interactionStatistic": {
-          "@type": "InteractionCounter",
-          "interactionType": { "@type": "WatchAction" },
-          "userInteractionCount": 2347
-        },
-        "expires": "2019-02-05T08:00:00+08:00"
-       }
+      
     }
     </script>
     <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
