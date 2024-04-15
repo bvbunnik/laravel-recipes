66caca765d824965704d37d3a69a3af6b8473415
Bram van Bunnik <bramvanbunnik@gmail.com>
2024-04-15 20:21:11 +0100

    first test

--- resources/views/recipes/recipe.blade.php
+++ resources/views/recipes/recipe.blade.php
@@ -1,6 +1,81 @@
 @extends('layouts.app')
 
 @section('content')
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "Recipe",
+      "name": @json($recipe->title),
+      "image": [
+        @json($recipe->photo)
+      ],
+      "description": @json($recipe->description),
+      "recipeCuisine": @json($recipe->cuisine->name),
+      "prepTime": "PT1M",
+      "cookTime": "PT2M",
+      "totalTime": "PT3M",
+      "keywords": "non-alcoholic",
+      "recipeYield": "4 servings",
+      "recipeCategory": "Drink",
+      "nutrition": {
+        "@type": "NutritionInformation",
+        "calories": "120 calories"
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "5",
+        "ratingCount": "18"
+      },
+      "recipeIngredient": [
+        "400ml of pineapple juice",
+        "100ml cream of coconut",
+        "ice"
+      ],
+      "recipeInstructions": [
+        {
+          "@type": "HowToStep",
+          "name": "Blend",
+          "text": "Blend 400ml of pineapple juice and 100ml cream of coconut until smooth.",
+          "url": "https://example.com/non-alcoholic-pina-colada#step1",
+          "image": "https://example.com/photos/non-alcoholic-pina-colada/step1.jpg"
+        },
+        {
+          "@type": "HowToStep",
+          "name": "Fill",
+          "text": "Fill a glass with ice.",
+          "url": "https://example.com/non-alcoholic-pina-colada#step2",
+          "image": "https://example.com/photos/non-alcoholic-pina-colada/step2.jpg"
+        },
+        {
+          "@type": "HowToStep",
+          "name": "Pour",
+          "text": "Pour the pineapple juice and coconut mixture over ice.",
+          "url": "https://example.com/non-alcoholic-pina-colada#step3",
+          "image": "https://example.com/photos/non-alcoholic-pina-colada/step3.jpg"
+        }
+      ],
+      "video": {
+        "@type": "VideoObject",
+        "name": "How to Make a Non-Alcoholic Piña Colada",
+        "description": "This is how you make a non-alcoholic piña colada.",
+        "thumbnailUrl": [
+          "https://example.com/photos/1x1/photo.jpg",
+          "https://example.com/photos/4x3/photo.jpg",
+          "https://example.com/photos/16x9/photo.jpg"
+         ],
+        "contentUrl": "https://www.example.com/video123.mp4",
+        "embedUrl": "https://www.example.com/videoplayer?video=123",
+        "uploadDate": "2018-02-05T08:00:00+08:00",
+        "duration": "PT1M33S",
+        "interactionStatistic": {
+          "@type": "InteractionCounter",
+          "interactionType": { "@type": "WatchAction" },
+          "userInteractionCount": 2347
+        },
+        "expires": "2019-02-05T08:00:00+08:00"
+       }
+    }
+    </script>
     <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-sm">
             <div class="modal-content">
